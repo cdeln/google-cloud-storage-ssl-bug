7cb86294ec6c008bddd3f6acb0db9f088f7d81cd
Carl Dehlin <carl.dehlin@rooniq.com>
2024-06-18 09:46:53 +0200

    Push docker image to cloud, upload from file instead of string, add workflow

--- build.sh
+++ build.sh
@@ -1,3 +1,29 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-docker build -t google-cloud-storage-ssl-error:latest .
+set -eu
+
+region=europe
+
+if [ $# -ne 2 ] ; then
+    echo "Usage: $0 artifact-repo <local|cloud>"
+    exit 1
+fi
+
+artifact_repo=$1
+build_mode=$2
+
+project_id=$(gcloud config get project)
+project_name=
+docker_image_url=$region-docker.pkg.dev/$project_id/$artifact_repo/google-cloud-storage-ssl-error:latest
+
+if [ $build_mode = local ]
+then
+    docker build --tag $docker_image_url .
+elif [ $build_mode == cloud ]
+then
+    $0 $artifact_repo local
+    docker push $docker_image_url
+else
+    echo "Invalid build mode $build_mode"
+    exit 1
+fi
--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+set -eu
+
+if [ $# -lt 1 ] ; then
+    echo "Usage: $0 artifact-repo [args...]"
+    exit 1
+fi
+
+artifact_repo=$1
+
 docker run \
        -v ~/.config/gcloud/application_default_credentials.json:/app/credentials.json \
        -e GOOGLE_APPLICATION_CREDENTIALS=/app/credentials.json \
        -e GCLOUD_PROJECT=$(gcloud config get project) \
-       google-cloud-storage-ssl-error:latest $@
+       europe-docker.pkg.dev/climbalyzer-420313/$artifact_repo/google-cloud-storage-ssl-error ${@:2}
